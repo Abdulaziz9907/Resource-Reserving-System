fae7a3a26b6b04e0c3a4d64825fb24925bbf9e0a
zyadRA <zyadmohmd009@gmail.com>
2024-05-11 22:24:44 +0300

    connection

--- src/main/java/OpenEvent/OpenEvent.java
+++ src/main/java/OpenEvent/OpenEvent.java
@@ -1,4 +1,27 @@
 package OpenEvent;
 
+import java.sql.*;
+
 public class OpenEvent {
+
+    public static void main(String[] args){
+        try {
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://127.0.0.1:3306/facilities",
+                    "root",
+                    "123123"
+            );
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM facilities.facility_table");
+
+            while (resultSet.next()){
+                System.out.println(resultSet.getString("name"));
+                System.out.println(resultSet.getString("location"));
+                System.out.println(resultSet.getString("isAvailable"));
+                System.out.println(resultSet.getString("capacity"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
