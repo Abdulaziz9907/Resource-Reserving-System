0877eed823eb4cd8eea8e7da6d029d8e965ae984
Abdulaziz9907 <abdullaziz.h7@gmail.com>
2024-05-14 16:41:52 +0300

    merged files

--- src/main/java/ReserveLabsClasses/Actions_LC.java
+++ src/main/java/ReserveLabsClasses/Actions_LC.java
@@ -1,4 +1,5 @@
 package ReserveLabsClasses;
+import Login.DB;
 import javafx.animation.TranslateTransition;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -48,7 +49,7 @@ public class Actions_LC implements Initializable {
     private Button Return_lC;
     @FXML
     private Label confirmationM;
-
+    private String gender = (String) DB.getGender();
     Parent root = null;
 
     @Override
@@ -75,7 +76,7 @@ public class Actions_LC implements Initializable {
                 if (Choice_CL.getValue() != null && !lC_Time_Start.getText().trim().isEmpty() && !lC_Time_End.getText().trim().isEmpty() && !roomNum.getText().trim().isEmpty() && !bldgNum.getText().trim().isEmpty() ) {
                     java.sql.Date sqlDate = Date.valueOf(date);
                     String selectedChoice = (String) Choice_CL.getValue();
-                    ReserveLabsClasses_DB.Reserve_CL(event, String.valueOf(selectedChoice),roomNum.getText(),bldgNum.getText(), sqlDate, lC_Time_Start.getText(),lC_Time_End.getText(), Details_lC.getText());
+                    ReserveLabsClasses_DB.Reserve_CL(event, Choice_CL.getValue(),roomNum.getText(),bldgNum.getText(), sqlDate,gender, lC_Time_Start.getText(),lC_Time_End.getText(), Details_lC.getText());
 
                     confirmationM.setText("The facility has been assigned successfully");
                 } else {
